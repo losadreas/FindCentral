97e5350400515bf2a9cc4da9d0d67055816245c5
losadreas <losadreas@gmail.com>
2022-12-23 11:29:59 +0200

    refactor

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-
-from functools import lru_cache
-class Image:
+class Central_Pixels_Finder(Image):
     def __init__(self, data, w, h):
         self.pixels = data
         self.width = w
         self.height = h
 
-
-class Central_Pixels_Finder(Image):
-    def __init__(self):
+    def central_pixels(self, colour):
         self.make_x_y_dict()
         self.make_info_by_position()
         self.adding_quantity()
-        self.dict_colors = self.make_dict_colors()
-        self.dict_points = self.find_central()
-
-    def central_pixels(self, colour):
+        self.make_dict_colors()
+        self.find_central()
         if colour in self.dict_points:
             return self.dict_points[colour]
         else:
@@ -30,11 +24,9 @@ class Central_Pixels_Finder(Image):
                 self.dict_image[line][row] = [image.pop(0)]
 
     def make_info_by_position(self):
-        row_max = self.width
-        line_max = self.height
         for line, row in self.dict_image.items():
             for r in row:
-                if line == 0 or line == line_max - 1 or r == 0 or r == row_max - 1:
+                if line == 0 or line == self.height - 1 or r == 0 or r == self.width - 1:
                     self.dict_image[line][r].append(0)
                 else:
                     color_cur = self.dict_image[line][r][0]
@@ -63,28 +55,25 @@ class Central_Pixels_Finder(Image):
                     self.dict_image[line][r].append(max_quantity)
 
     def make_dict_colors(self):
-        dict_colors = {}
+        self.dict_colors = {}
         for line, row in self.dict_image.items():
             for r in row:
                 color = self.dict_image[line][r][0]
                 quantity = self.dict_image[line][r][2]
-                if color in dict_colors:
-                    dict_colors[color].add(quantity)
+                if color in self.dict_colors:
+                    self.dict_colors[color].add(quantity)
                 else:
-                    dict_colors[color] = {quantity}
-        for color in dict_colors:
-            dict_colors[color] = max(dict_colors[color])
-        return dict_colors
+                    self.dict_colors[color] = {quantity}
+        for color in self.dict_colors:
+            self.dict_colors[color] = max(self.dict_colors[color])
 
     def find_central(self):
-        dict_points = {}
-        row_max = self.width
+        self.dict_points = {}
         for line, row in self.dict_image.items():
             for r in row:
                 color = self.dict_image[line][r][0]
                 if self.dict_image[line][r][2] == self.dict_colors[color]:
-                    if color in dict_points:
-                        dict_points[color].append(line * row_max + r)
+                    if color in self.dict_points:
+                        self.dict_points[color].append(line * self.width + r)
                     else:
-                        dict_points[color] = [line * row_max + r]
-        return dict_points
+                        self.dict_points[color] = [line * self.width + r]
